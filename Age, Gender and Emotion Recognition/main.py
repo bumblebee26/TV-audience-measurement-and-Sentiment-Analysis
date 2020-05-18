2c76f030698038f1e2ea7ef0968adeae2668866d
Viren <virenbaria17@gmail.com>
2020-05-18 09:50:32 +0530

    Added video writer

--- Age, Gender and Emotion Recognition/main.py
+++ Age, Gender and Emotion Recognition/main.py
@@ -29,13 +29,17 @@ key = ['Channel_id', 'House_id', 'User_id', 'Timestamp', 'Age', 'Gender', 'Emoti
 cam = cv2.VideoCapture(0)
 (H, W) = (None, None)
 
+# Creating Video writer to save video // Keep the video writer frame dimensions same as the final frame dimensions// 
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('recognition.avi', fourcc, 10, (400, 350))
+
 # The While loop 
 while True:
     ret, frame = cam.read()
     if ret is False:
         break
 
-    frame = cv2.resize(frame, (400, 400))
+    frame = cv2.resize(frame, (400, 350))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     if W is None or H is None:
@@ -87,16 +91,16 @@ while True:
             for (i, (k, v)) in enumerate(content):
                 text = "{}: {}".format(k, v) 
                 cv2.putText(frame, text, (startx, starty - i*10 - 5),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 1)
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 1)
             cv2.putText(frame, "Id {}".format(user_id), (cent[0] - 20, cent[1] - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
 
 
             # Sending data to database
             entry = [1, 1, user_id, str(datetime.now()), age, gender, label]
             data.append(dict(zip(key, entry)))
-            print(dict(zip(key, entry)))
-            print("\n")
+            # print(dict(zip(key, entry)))
+            # print("\n")
 
             r = random.randint(0,255); g = random.randint(0,255); b = random.randint(0,255)
         except:
@@ -105,13 +109,16 @@ while True:
 
         cv2.rectangle(frame, (startx, starty), (endx, endy), (r, g, b), 2)
 
+    # out.write(frame)
     cv2.imshow("People's Meter", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
     
+# out.release()
 cam.release()
 cv2.destroyAllWindows()
 
+# Create a .csv file of the data 
 csv_file = "People's meter data.csv"
 try:
     with open(csv_file, 'w') as csvfile:
@@ -119,5 +126,6 @@ try:
         writer.writeheader()
         for dict in data:
             writer.writerow(dict)
+    print(".csv file is created")
 except IOError:
     print("I/O error")
